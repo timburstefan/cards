0af9679380e729ac3e4cf1816947f9cec30750a9
stefan.timbur <stefan.timbur@lightcyphers.com>
2023-08-06 23:01:27 +0300

    added docs and testing

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    This module contains functions for creating, shuffling, and dealing a deck of cards.
+  """
   @deckname "deck"
+  @doc """
+    Returns a list of strings representing a deck of cards.
+  """
   def create_deck do
     values = [
       "Ace",
@@ -21,23 +27,55 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Returns a list of strings representing a shuffled deck of cards.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Returns true if the deck contains the hand, false otherwise.
+  
+  ## Examples
+  
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, "Ace of Clubs")
+      true
+  
+  """
   def contains?(deck, hand) do
     Enum.member?(deck, hand)
   end
+
+  @doc """
+    Returns a tuple containing the hand and the remaining deck.
+    The `hand_size` represents the number of cards in the hand.
+  
+    ## Examples
+  
+      iex> deck = Cards.create_deck()
+      iex> {hand, _remaining_deck} = Cards.deal(deck, 3)
+      iex> hand
+      ["Ace of Clubs", "Two of Clubs", "Three of Clubs"]
+  
+  """
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Saves the deck to a file.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Loads the deck from a file.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
@@ -45,10 +83,18 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Creates a hand from a shuffled deck.
+  """
+  def create_hand do
+    create_deck() |> shuffle() |> deal(5)
+  end
+
+  @doc """
+    Executes the program.
+  """
   def execute do
-    Cards.create_deck() |> Cards.save(@deckname)
-    Cards.load(@deckname) |> IO.inspect()
+    create_hand() |> save(@deckname)
+    load(@deckname) |> IO.inspect()
   end
 end
-
-Cards.execute()
